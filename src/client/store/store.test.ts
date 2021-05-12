21fa27a9404b65332267f6032f15d0f983df6440
Tevin Banton <67722040+TevinAB@users.noreply.github.com>
2021-05-12 10:47:40 -0500

    updated store tests (#11)

--- src/client/store/store.test.ts
+++ src/client/store/store.test.ts
@@ -1,18 +1,13 @@
-import { Subscriber, State } from './../types';
+import { CartItem, State } from '@/types';
 import Store from './store';
 
-class TestObj implements Subscriber {
-  update = jest.fn();
-}
-
 describe('Tests for the Store object', () => {
-  let subscriber: TestObj;
   let store: Store;
-  let state: {};
+  let state: { shoppingCart: Array<CartItem> };
   const TEST_ID = 'apl12';
+  let callBack: jest.Mock<any, any>;
 
   beforeEach(() => {
-    subscriber = new TestObj();
     state = {
       shoppingCart: [
         {
@@ -32,33 +27,30 @@ describe('Tests for the Store object', () => {
       ],
     };
     store = new Store(state as State);
+    callBack = jest.fn();
   });
 
   it('should accept subscribers', () => {
     expect(store).not.toBe(null);
     expect(store.getSubscriberCount()).toBe(0);
 
-    store.subscribe(subscriber);
+    store.subscribe('ADD_ITEM', callBack);
     expect(store.getSubscriberCount()).toBe(1);
   });
 
   it('should allow subscribers to unsubscribe', () => {
-    const cleanUp = store.subscribe(subscriber);
+    const cleanUp = store.subscribe('ADD_ITEM', callBack);
+
     expect(store.getSubscriberCount()).toBe(1);
     cleanUp();
     expect(store.getSubscriberCount()).toBe(0);
   });
 
   it('should allow state updates', () => {
-    store.updateState((state: State) => {
-      return {
-        shoppingCart: state.shoppingCart.map((cartItem) => {
-          if (cartItem.id !== TEST_ID) return cartItem;
+    const newState = { ...state };
+    newState.shoppingCart[0].quantity = 2;
 
-          return { ...cartItem, quantity: cartItem.quantity + 1 };
-        }),
-      };
-    });
+    store.setState('ITEM_QTY_CHANGE', newState as State);
 
     expect(store.getState().shoppingCart[0].quantity).toBe(2);
     expect(store.getState().shoppingCart[0].pricePerUnit).toBe(12.1);
@@ -66,19 +58,18 @@ describe('Tests for the Store object', () => {
   });
 
   it('should update all subscribers when there is a change in state', () => {
-    store.subscribe(subscriber);
+    const callBack2 = jest.fn();
+    store.subscribe('ITEM_QTY_CHANGE', callBack);
+    store.subscribe('ADD_ITEM', callBack2);
 
-    store.updateState((state: State) => {
-      return {
-        shoppingCart: state.shoppingCart.map((cartItem) => {
-          if (cartItem.id !== TEST_ID) return cartItem;
+    const newState = { ...state };
+    newState.shoppingCart[0].quantity = 2;
 
-          return { ...cartItem, quantity: cartItem.quantity + 1 };
-        }),
-      };
-    });
+    store.setState('ITEM_QTY_CHANGE', newState as State);
 
-    expect(subscriber.update.mock.calls.length).toBe(1);
+    expect(callBack.mock.calls.length).toBe(1);
+    expect(callBack.mock.calls[0][0]).toEqual(newState);
+    expect(callBack2.mock.calls.length).toBe(0);
   });
 
   it('should allow its state to be read', () => {
