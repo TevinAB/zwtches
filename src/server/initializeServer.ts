c854d5e7cd61e235290bb4cf1c8ae81f2cf3ecb2
Tevin Banton <tevinanthony95@gmail.com>
2021-06-03 17:28:50 -0500

    setup server to serve static file

--- src/server/initializeServer.ts
+++ src/server/initializeServer.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import api from './routes';
+import path from 'path';
 
 const app = express();
 
-app.use('/api', api);
+app.use(express.static(path.join(__dirname, '../../dist/')));
 app.use(express.json());
+app.use('/api', api);
+app.use('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../dist/', 'index.html'));
+});
 
 export default app;
